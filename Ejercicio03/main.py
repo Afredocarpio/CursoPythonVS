ac22d9ea2b99f94545907c08ffb7de0976862009
Alfredo Carpio <85145624+Afredocarpio@users.noreply.github.com>
2022-08-10 11:04:48 -0500

    Update main.py

--- Ejercicio03/main.py
+++ Ejercicio03/main.py
@@ -1,9 +1,6 @@
 """Realiza un programa con los siguientes requisitos:
-
 Debes leer por teclado dos cadenas de caracteres, una llamada nombre y otra llamada apellido.
-
 Debes leer por teclado un número entero llamado edad (recuerda que la variable debe ser de tipo entero).
-
 Debes leer por teclado un número flotante llamado numero_magico.
 
 Finalmente debes crear una variable cadena_final con el siguiente formato:
