e043d3839272479b530e64b35a1f61e96c5bd8d4
Alfredo Carpio <85145624+Afredocarpio@users.noreply.github.com>
2022-08-10 10:48:13 -0500

    Update main.py

--- Ejercicio01/main.py
+++ Ejercicio01/main.py
@@ -1,24 +1,18 @@
-"""Al realizar una consulta en un registro hemos obtenido una cadena de texto al revés. Al parecer contiene el nombre de un alumno, la nota de un exámen y la materia. Realiza lo siguiente:
-
-Voltea la cadena y guárdala en una variable llamada cadena_volteada. Puedes devolver una cadena volteada usando un tercer índice negativo con slicing tal que así: cadena[::-1]
-
+"""Al realizar una consulta en un registro hemos obtenido una cadena de texto al revés. Al parecer contiene el nombre de un alumno, la nota de un exámen y la materia. 
+Realiza lo siguiente:
+Voltea la cadena y guárdala en una variable llamada cadena_volteada. Puedes devolver una cadena volteada usando un tercer índice negativo con slicing tal que así:
+cadena[::-1]
 Extrae el nombre y apellido del alumno y almacénalos en una variable llamada nombre.
-
 Extrae la nota y almacénala en una variable llamada nota.
-
 Extrae la materia y almacénala en una variable llamada materia.
-
-Genera exactamente la siguiente estructura formateando las anteriores variables en una cadena llamada cadena_formateada donde NOMBRE, APELLIDO, NOTA y MATERIA hacen referencia a las variables extraídas:
-
+Genera exactamente la siguiente estructura formateando las anteriores variables en una cadena llamada cadena_formateada donde NOMBRE, APELLIDO, NOTA y MATERIA
+hacen referencia a las variables extraídas:
 NOMBRE APELLIDO ha sacado un NOTA en MATERIA
+
 Notas y consejos:
-
 Todas las variables del enunciado deben existir y contener el valor correcto para pasar las pruebas.
-
 Utiliza slicing para extraer las porciones de la cadena que te interesan.
-
 Crea tantas variables como necesites para hacerlo más sencillo.
-
 Utiliza el formateo de variables todo lo que puedas."""
 
 
